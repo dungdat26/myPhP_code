dd1db9efdee72576d7db66d9b31ead7d8391b2f2
dungdat26 <71504165+dungdat26@users.noreply.github.com>
2020-10-20 01:26:17 +0700

    .

--- buoi9/index.php
+++ buoi9/index.php
@@ -1,13 +1,20 @@
-<?php 
-if(isset ($_GET['page'])){
+<?php
+include_once('./libraries/function_support.php');
+ 
+include_once('./widgets/head.php');
+include_once('./widgets/header.php');
+
+if(isset($_GET['page'])){
     if($_GET['page'] == 'sach'){
-     include_once('./admin/pages/ds_sach.php');
+        include_once('./pages/ds_sach.php');
     }
     else{
         include_once('./pages/trang_chu.php');
     }
 }
-else{
-    include_once('./admin/pages/dangnhap.php');
+else {
+    include_once('./pages/trang_chu.php');
 }
+
+
 ?>
--- buoi9/model/xl_sach.php
+++ buoi9/model/xl_sach.php
@@ -1,19 +1,45 @@
-<?php 
+<?php
 include_once('./model/database.php');
+
 class xl_sach extends database{
+
     function xl_sach(){
         parent::database();
     }
 
-    
-    function get_sach(){
+    function ds_sach_noi_bat(){
         $string_sql = "SELECT s.*, ten_tac_gia 
                         FROM bs_sach s JOIN bs_tac_gia tg ON s.id_tac_gia = tg.id 
-                        WHERE noi_bat=1";
+                        WHERE noi_bat = 1";
         $this->setSQL($string_sql);
         $this->execute();
-       $result = $this->loadAllRow();
-       return $result;
+        $result = $this->loadAllRow();
+        return $result;
     }
+
+    function ds_sach_moi(){
+        $string_sql = "SELECT s.*, ten_tac_gia 
+                        FROM bs_sach s JOIN bs_tac_gia tg ON s.id_tac_gia = tg.id 
+                        ORDER BY id DESC
+                        LIMIT 8";
+        $this->setSQL($string_sql);
+        $this->execute();
+        $result = $this->loadAllRow();
+        return $result;
+    }
+
+    function ds_sach_ban_chay(){
+        $string_sql = "SELECT s.*, ten_tac_gia , SUM(so_luong) as so_luong_tong
+                        FROM bs_sach s JOIN bs_tac_gia tg ON s.id_tac_gia = tg.id 
+                        JOIN bs_chi_tiet_don_hang ctdh ON ctdh.id_sach = s.id
+                        GROUP BY s.id
+                        ORDER BY so_luong_tong DESC
+                        LIMIT 8";
+        $this->setSQL($string_sql);
+        $this->execute();
+        $result = $this->loadAllRow();
+        return $result;
+    }
+
 }
 ?>
